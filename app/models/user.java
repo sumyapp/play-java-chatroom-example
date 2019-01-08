ea78afdef466e04f5397fd77cf3fb3f6bea43fc9
Soutaro Matsumoto <matsumoto@soutaro.com>
2019-01-08 10:00:48 +0900

    Fix model

--- app/models/user.java
+++ app/models/user.java
@@ -3,6 +3,7 @@ package models;
 import javax.persistence.*;
 import play.db.ebean.*;
 import com.avaje.ebean.*;
+import java.net.URL;
 
 @Entity
 public class User extends Model {
@@ -11,14 +12,25 @@ public class User extends Model {
     public String email;
     public String name;
     public String password;
+    public String avatarURL;
     
-    public User(String email, String name, String password) {
+    public User(String email, String name, String password, String avatarURL) {
       this.email = email;
       this.name = name;
       this.password = password;
+      this.avatarURL = avatarURL;
     }
 
     public static Finder<String,User> find = new Finder<String,User>(
         String.class, User.class
-    ); 
+    );
+
+    public URL getAvatarURL() {
+      String url = this.avatarURL;
+      if (this.avatarURL) {
+        return new URL(this.avatarURL);
+      } else {
+        return null;
+      }
+    }
 }
